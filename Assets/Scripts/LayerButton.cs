492c39db7bffc3e1c32127e8783f05b41b4dc1f2
Pauleen Lam <pauleen.lam@outlook.com>
2022-04-07 23:43:26 -0400

    Set up UI and Cube Sets

---
+++ Assets/Scripts/LayerButton.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+}
